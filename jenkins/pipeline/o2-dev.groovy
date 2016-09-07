28b336caed421eb807ff7671bc866a32bbf01c54
Garrett Potts <gpotts@radiantblue.com>
2016-09-07 16:01:17 -0400

    Added new o2-dev dsl

---
+++ jenkins/pipeline/o2-dev.groovy
@@ -0,0 +1,35 @@
+node {
+   env.WORKSPACE=pwd()
+   stage("Checkout"){
+       dir("omar"){
+          git branch: 'dev', url: 'https://github.com/ossimlabs/omar.git'
+       }
+       dir("ossim-ci"){
+          git branch: 'dev-OCS626', url: 'https://github.com/ossimlabs/ossim-ci.git'
+       }
+   }
+   echo "${env.WORKSPACE}"
+   stage("Download Artifacts"){
+       dir("${env.WORKSPACE}/install/share/java"){
+           step ([$class: 'CopyArtifact',
+              projectName: 'ossim-dev',
+              filter: "artifacts/*.jar",
+              flatten: true])
+       }
+   }
+   stage("Build"){
+       sh """
+         ${env.WORKSPACE}/ossim-ci/scripts/linux/o2-build.sh
+         ${env.WORKSPACE}/ossim-ci/scripts/linux/o2-install.sh
+       """
+   }
+   stage("Test"){
+     echo "NEED TO ADD TESTS FOR THE INSTALLATION!!!!"
+   }
+   
+   stage("Archive"){
+     dir("${env.WORKSPACE}"){
+         archiveArtifacts 'install.tgz'
+     }
+   }
+}
