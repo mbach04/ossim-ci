32cb0011bce5c3f3086ee4cde80b4dde018904dc
Garrett Potts <gpotts@radiantblue.com>
2016-09-07 07:49:38 -0400

    Added more build variables so we can remove from the CI

--- scripts/linux/ossim-build.sh
+++ scripts/linux/ossim-build.sh
@@ -3,19 +3,26 @@
 pushd `dirname $0` >/dev/null
 export SCRIPT_DIR=`pwd -P`
 pushd $SCRIPT_DIR/../../.. >/dev/null
-export OSSIM_DEV_HOME=$PWD
+#export OSSIM_DEV_HOME=$PWD
 popd > /dev/null
 popd >/dev/null
 
 source $SCRIPT_DIR/ossim-env.sh
 
-pushd $OSSIM_DEV_HOME/ossim-oms/joms/build_scripts/linux
-./setup.sh
-popd
+if [ "$BUILD_OMS" = "ON" ] ; then
+   pushd $OSSIM_DEV_HOME/ossim-oms/joms/build_scripts/linux
+   ./setup.sh
+   popd
+fi
 
-pushd $OSSIM_DEV_HOME/ossim/scripts
-./build.sh
-popd
+if [ -d $OSSIM_DEV_HOME/ossim ] ; then
+   pushd $OSSIM_DEV_HOME/ossim/scripts
+   ./build.sh
+   popd
+else
+   echo "ERROR: OSSIM module not found.  We at least need ossim module to build the baseline"
+   exit 1
+fi
 
 pushd $OSSIM_DEV_HOME/ossim-oms/joms/build_scripts/linux
 ./build.sh
