d46f83bad915c407baf575d5b73065316e11e06e
Garrett Potts <gpotts@radiantblue.com>
2016-09-07 16:24:52 -0400

    Added the proper paths

--- scripts/linux/oldmar-build.sh
+++ scripts/linux/oldmar-build.sh
@@ -14,14 +14,14 @@ if [ ! -f $OSSIM_INSTALL_PREFIX/share/java/joms-$OSSIM_VERSION.jar ]; then
 fi
 # make sure the joms jar is in the local maven repo
 mvn install:install-file -Dfile=$OSSIM_DEV_HOME/install/share/java/joms-$OSSIM_VERSION.jar -DgroupId=org.ossim -DartifactId=joms -Dversion=$OSSIM_VERSION -Dpackaging=jar
-mv $OSSIM_DEV_HOME/install ossim-install
+mv $OSSIM_INSTALL_PREFIX ossim-install
 
 if [ $? -ne 0 ]; then
  echo; echo "ERROR: MVN install failed for joms."
  exit 1
 fi
 
-$ROOT_DIR/omar/build_scripts/linux/build.sh
+$OSSIM_DEV_HOME/omar/build_scripts/linux/build.sh
 if [ $? -ne 0 ]; then
  echo; echo "ERROR: OMAR failed to build."
  exit 1
