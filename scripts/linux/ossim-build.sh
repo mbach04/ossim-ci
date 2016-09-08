1d18c23c212018804861f0d1c6e630200666bc38
Garrett Potts <gpotts@radiantblue.com>
2016-09-08 08:42:48 -0400

    Fixed error checking

--- scripts/linux/ossim-build.sh
+++ scripts/linux/ossim-build.sh
@@ -11,12 +11,20 @@ source $SCRIPT_DIR/ossim-env.sh
 if [ "$BUILD_OMS" = "ON" ] ; then
    pushd $OSSIM_DEV_HOME/ossim-oms/joms/build_scripts/linux
    ./setup.sh
+   if [ $? -ne 0 ]; then
+     echo; echo "ERROR: Build SETUP for OSSIM."
+     exit 1
+   fi
    popd
 fi
 
 if [ -d $OSSIM_DEV_HOME/ossim ] ; then
    pushd $OSSIM_DEV_HOME/ossim/scripts
    ./build.sh
+   if [ $? -ne 0 ]; then
+     echo; echo "ERROR: Build failed for OSSIM."
+     exit 1
+   fi
    popd
 else
    echo "ERROR: OSSIM module not found.  We at least need ossim module to build the baseline"
@@ -25,11 +33,8 @@ fi
 
 pushd $OSSIM_DEV_HOME/ossim-oms/joms/build_scripts/linux
 ./build.sh
+   if [ $? -ne 0 ]; then
+     echo; echo "ERROR: Build failed for JOMS."
+     exit 1
+   fi
 popd
-
-if [ -d $OSSIM_DEV_HOME/oldomar ]; then
-   pushd $OSSIM_DEV_HOME/oldomar/build_scripts/linux
-   ./build.sh
-   popd
-fi
-
