5861f624138858cc01eae876d38d664c6cbcf3a8
Garrett Potts <gpotts@radiantblue.com>
2016-09-10 16:43:20 -0400

    Added the ability to copy out binary dependencies

--- scripts/linux/functions.sh
+++ scripts/linux/functions.sh
@@ -30,3 +30,50 @@ function getOsInfo {
 function getTimeStamp {
         eval $1=`date +%Y-%m-%d-%H%M`
 }
+
+
+function copyDependencies {
+    #!/bin/bash
+
+    if [ $# != 2 ] ; then
+        echo "usage: copyDependencies PATH_TO_BINARY TARGET_FOLDER"
+        return 1
+    fi
+
+    PATH_TO_BINARY="$1"
+    TARGET_FOLDER="$2"
+
+    # if we cannot find the the binary we have to abort
+    if [ ! -f "$PATH_TO_BINARY" ] ; then
+        echo "The file '$PATH_TO_BINARY' was not found. Aborting!"
+        return 1
+    fi
+
+    mkdir -p $TARGET_FOLDER/{bin, lib64, lib}
+    
+    # copy the binary to the target folder
+    # create directories if required
+    echo "---> copy binary itself"
+    cp --parents -v "$PATH_TO_BINARY" "$TARGET_FOLDER"
+
+    # copy the required shared libs to the target folder
+    # create directories if required
+    echo "---> copy libraries"
+    for lib in `ldd "$PATH_TO_BINARY" | cut -d'>' -f2 | awk '{print $1}'` ; do
+       if [ -d "$lib" ] ; then
+          mkdir -p "$lib"
+       fi
+       if [ -f "$lib" ] ; then
+            cp -v --parents "$lib" "$TARGET_FOLDER"
+       fi
+    done
+
+    # I'm on a 64bit system at home. the following code will be not required on a 32bit system.
+    # however, I've not tested that yet
+    # create lib64 - if required and link the content from lib to it
+    if [ ! -d "$TARGET_FOLDER/lib64" ] ; then
+        mkdir -v "$TARGET_FOLDER/lib64"
+    fi
+}
+
+
