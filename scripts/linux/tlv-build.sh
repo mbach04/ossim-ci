dbcac966b52bcb94b3a0800bf121bd474b0b964f
Garrett Potts <gpotts@radiantblue.com>
2016-09-16 08:41:03 -0400

    Added error chewck for the build

--- scripts/linux/tlv-build.sh
+++ scripts/linux/tlv-build.sh
@@ -10,4 +10,9 @@ source $SCRIPT_DIR/ossim-env.sh
 
 pushd $OSSIM_DEV_HOME/tlv/time_lapse
 ./gradlew assemble
+if [ $? -ne 0 ]; then
+ echo; echo "ERROR: Unable to buld Time Lapse Viewer."
+ exit 1
+fi
 
+popd
