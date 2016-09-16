888a5869bc85a251a3626b4e7ed8781be87d88b2
Garrett Potts <gpotts@radiantblue.com>
2016-09-16 08:28:10 -0400

    Added build

---
+++ scripts/linux/tlv-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# Set GoCD-specific environment:
+pushd `dirname $0` >/dev/null
+export SCRIPT_DIR=`pwd -P`
+pushd $SCRIPT_DIR/../../.. >/dev/null
+popd > /dev/null
+popd >/dev/null
+
+source $SCRIPT_DIR/ossim-env.sh
+
