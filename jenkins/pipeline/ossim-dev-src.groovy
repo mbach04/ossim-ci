2e3cefa780fba98fca3caa906ca4b2e39635eba5
Garrett Potts <gpotts@radiantblue.com>
2016-09-20 13:15:25 -0400

    Using generic variable OSIMS_GIT_BRANCH

--- jenkins/pipeline/ossim-dev-src.groovy
+++ jenkins/pipeline/ossim-dev-src.groovy
@@ -1,45 +1,45 @@
 node {
     env.WORKSPACE=pwd()
     stage("Checkout"){
-      dir("ossim-dev-src"){
+      dir("ossim-${OSSIM_GIT_BRANCH}-src"){
         dir("ossim"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim.git'
         }
         dir("ossim-video"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-video.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-video.git'
         }
         dir("ossim-planet"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-planet.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-planet.git'
         }
         dir("ossim-gui"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-gui.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-gui.git'
         }
         dir("ossim-oms"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-oms.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-oms.git'
         }
         dir("ossim-plugins"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-plugins.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-plugins.git'
         }
         dir("ossim-wms"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-wms.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-wms.git'
         }
         dir("ossim-ci"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-ci.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
         }
         dir("ossim-private"){
-           git branch: 'dev', url: 'git@ossim-private.github.com:radiantbluetechnologies/ossim-private.git'
+           git branch: "${OSSIM_GIT_BRANCH}", url: 'git@ossim-private.github.com:radiantbluetechnologies/ossim-private.git'
         }
         dir("omar"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/omar.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/omar.git'
         }
         dir("oldomar"){
-          git branch: 'dev', url: 'https://github.com/radiantbluetechnologies/omar.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/radiantbluetechnologies/omar.git'
         }
         dir("ossim-ci"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/ossim-ci.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
         }
         dir("ossimlabs-tlv"){
-          git branch: 'dev', url: 'https://github.com/ossimlabs/tlv.git'
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/tlv.git'
         }
         dir("tlv"){
           git branch: 'master', url: 'https://github.com/time-lapse-viewer/tlv.git'
@@ -48,13 +48,13 @@ node {
     }
    stage("Archive"){
      dir("${env.WORKSPACE}"){
-         dir("ossim-dev-src"){
+         dir("ossim-${OSSIM_GIT_BRANCH}-src"){
           sh "rm -rf `find . -name .git`"
          }
-         sh "tar cvfz ossim-dev-src.tgz ossim-dev-src"
+         sh "tar cvfz ossim-${OSSIM_GIT_BRANCH}-src.tgz ossim-${OSSIM_GIT_BRANCH}-src"
      }
      dir("${env.WORKSPACE}/artifacts"){
-         sh "mv ${env.WORKSPACE}/ossim-dev-src.tgz ."
+         sh "mv ${env.WORKSPACE}/ossim-${OSSIM_GIT_BRANCH}-src.tgz ."
      }
      archiveArtifacts 'artifacts/*'
   }
