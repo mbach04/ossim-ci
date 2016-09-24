ee18b9bd2ad8af7158fb638781e581b45cad319f
Garrett Potts <gpotts@radiantblue.com>
2016-09-24 17:04:21 -0400

    Removed the class definitions

--- jenkins/pipeline/notify.groovy
+++ jenkins/pipeline/notify.groovy
@@ -1,13 +1,11 @@
-Class Notify{
-   def notifyFailed() {
-     slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
-     emailext (
-         subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-         body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
-         recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-       )
-   }
-  
+def notifyFailed() {
+  slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+  emailext (
+      subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
 }
+
 
