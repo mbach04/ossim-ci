b09f0ebde38c9f4a8f74f33de8d630a233bd7469
Garrett Potts <gpotts@radiantblue.com>
2016-09-24 17:26:32 -0400

    Added notify for errors

--- jenkins/pipeline/oldmar.groovy
+++ jenkins/pipeline/oldmar.groovy
@@ -1,39 +1,48 @@
+def notifyObj
 node {
     env.WORKSPACE=pwd()
-    stage("Checkout"){
-        dir("omar"){
-          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/radiantbluetechnologies/omar.git'
-        }
-       dir("ossim-ci"){
-          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
-       }
-    }
-   stage("Download Artifacts"){
+    try{
+      stage("Checkout"){
+          dir("omar"){
+            git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/radiantbluetechnologies/omar.git'
+          }
+         dir("ossim-ci"){
+            git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
+         }
+         notifyObj = load "ossim-ci/jenkins/pipeline/notify.groovy"
+      }
+     stage("Download Artifacts"){
+         dir("${env.WORKSPACE}"){
+             step ([$class: 'CopyArtifact',
+                projectName: "ossim-${OSSIM_GIT_BRANCH}",
+                filter: "artifacts/*.jar",
+                flatten: true])
+         }
+     }
+     stage("Build"){
+         sh """
+           ${env.WORKSPACE}/ossim-ci/scripts/linux/oldmar-build.sh
+         """
+     }
+     stage("Install"){
+           sh "${env.WORKSPACE}/ossim-ci/scripts/linux/oldmar-install.sh"
+     }
+     stage("Archive"){
        dir("${env.WORKSPACE}"){
-           step ([$class: 'CopyArtifact',
-              projectName: "ossim-${OSSIM_GIT_BRANCH}",
-              filter: "artifacts/*.jar",
-              flatten: true])
+           sh "tar cvfz install.tgz install"
        }
-   }
-   stage("Build"){
-       sh """
-         ${env.WORKSPACE}/ossim-ci/scripts/linux/oldmar-build.sh
-       """
-   }
-   stage("Install"){
-         sh "${env.WORKSPACE}/ossim-ci/scripts/linux/oldmar-install.sh"
-   }
-   stage("Archive"){
-     dir("${env.WORKSPACE}"){
-         sh "tar cvfz install.tgz install"
-     }
-     dir("${env.WORKSPACE}/artifacts"){
-         sh "mv ${env.WORKSPACE}/install.tgz ."
-     }
-     archiveArtifacts 'artifacts/*'
+       dir("${env.WORKSPACE}/artifacts"){
+           sh "mv ${env.WORKSPACE}/install.tgz ."
+       }
+       archiveArtifacts 'artifacts/*'
+    }
+    stage("Clean Workspace"){
+      step([$class: 'WsCleanup'])
+    }
   }
-  stage("Clean Workspace"){
-    step([$class: 'WsCleanup'])
+  catch(e)
+  {
+    currentBuild.result = "FAILED"
+    notifyObj?.notifyFailed()
   }
 }
