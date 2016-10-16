25bfeba84b99ed8e4c38878977f56f464c0287a6
Garrett Potts <gpotts@radiantblue.com>
2016-10-16 18:44:59 -0400

    Commenting out the histogram until I can revisit

--- scripts/linux/ossim-test.sh
+++ scripts/linux/ossim-test.sh
@@ -57,7 +57,7 @@ fi
 
 # Copy the ossim preferences file to the top install directory:
 echo; echo "STATUS: Copying ossim preferences to install directory...";
-runCommand "cp $WORKSPACE/ossim-ci/batch_tests/ossim.config $OSSIM_INSTALL_PREFIX"
+runCommand "cp $OSSIM_DEV_HOME/ossim-ci/batch_tests/ossim.config $OSSIM_INSTALL_PREFIX"
 
 # Do basic ossim config and version check first:
 echo; echo "STATUS: Running ossim-info --config test...";
