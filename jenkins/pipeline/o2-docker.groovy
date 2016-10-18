c2af63f8e6276e808e551103fc411b4fed295ae7
Garrett Potts <gpotts@radiantblue.com>
2016-10-18 08:32:01 -0400

    Added error check and notification on failures and uncommented out the workspace cleanup

--- jenkins/pipeline/o2-docker.groovy
+++ jenkins/pipeline/o2-docker.groovy
@@ -1,21 +1,33 @@
+def notifyObj
 node("o2.radiantbluecloud.com"){
    env.WORKSPACE=pwd()
    env.DOCKER_HOST_URL="${DOCKER_HOST_URL}"
-   stage("Checkout"){
-       dir("omar"){
-          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/omar.git'
-       }
-       dir("ossim-ci"){
-          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
-       }
+   try{
+
+     stage("Checkout"){
+         dir("omar"){
+            git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/omar.git'
+         }
+         dir("ossim-ci"){
+            git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
+         }
+        notifyObj = load "${env.WORKSPACE}/ossim-ci/jenkins/pipeline/notify.groovy"
+    }
+     stage("Confiure Docker"){
+         //dir("${env.WORKSPACE}/omar/build_scripts/docker"){
+             sh """
+             ${env.WORKSPACE}/ossim-ci/scripts/linux/o2-docker.sh
+             """
+         //}
+     }
    }
-   stage("Confiure Docker"){
-       //dir("${env.WORKSPACE}/omar/build_scripts/docker"){
-           sh """
-           ${env.WORKSPACE}/ossim-ci/scripts/linux/o2-docker.sh
-           """
-       //}
+   catch(e)
+   {
+      println e
+      currentBuild.result = "FAILED"
+      notifyObj?.notifyFailed()
    }
+
    stage("Clean Workspace"){
      step([$class: 'WsCleanup'])
   }
