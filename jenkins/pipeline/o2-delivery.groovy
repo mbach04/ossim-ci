d8ccad506c843e377356bdf4159e3231f4a50681
Garrett Potts <gpotts@radiantblue.com>
2016-11-15 12:57:48 -0500

    Adding definitions for o2-delivery

--- jenkins/pipeline/o2-delivery.groovy
+++ jenkins/pipeline/o2-delivery.groovy
@@ -1,14 +1,17 @@
 env.WORKSPACE=pwd()
+env.S3_DELIVERY_BUCKET="s3://o2-delivery/${OSSIM_GIT_BRANCH}"
+env.DOCKER_REGISTRY_URI="320588532383.dkr.ecr.us-east-1.amazonaws.com"
 
 node("docker_exporter"){
-   env.S3_DELIVERY_BUCKET="s3://o2-delivery/${OSSIM_GIT_BRANCH}"
-   env.DOCKER_REGISTRY_URI="320588532383.dkr.ecr.us-east-1.amazonaws.com"
 
    echo "WORKSPACE        = ${env.WORKSPACE}"
    echo "S3_DELIVERY_BUCKET   = ${env.S3_DATA_BUCKET}"
    stage("Checkout") {
        dir("omar") {
           git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/omar.git'
+       }
+       dir("ossim-ci") {
+          git branch: "${OSSIM_GIT_BRANCH}", url: 'https://github.com/ossimlabs/ossim-ci.git'
        }
    }
 
