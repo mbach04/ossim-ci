bee08e7b25bd06a429de5a77a2f09cda06a31a02
Oscar Kramer <oscarkramer@users.noreply.github.com>
2016-12-29 11:45:54 -0500

    Update o2-paas-docker-image-build.groovy

--- jenkins/pipeline/o2-paas-docker-image-build.groovy
+++ jenkins/pipeline/o2-paas-docker-image-build.groovy
@@ -1,17 +1,14 @@
 node{
     env.WORKSPACE=pwd()
-
+    env.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+    env.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
     if (USE_C2S_ACCOUNT=="true") {
         echo "Using C2S account"
         env.USE_C2S_ACCOUNT="true"
-        env.AWS_ACCESS_KEY_ID="AKIAIRUUC4YXIJQXK7AQ"
-        env.AWS_SECRET_ACCESS_KEY="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"
     }
     else {
         echo "Using ModApps account"
         env.USE_C2S_ACCOUNT="false"
-        env.AWS_ACCESS_KEY_ID="AKIAJKEVNXHCKJETW2QQ"
-        env.AWS_SECRET_ACCESS_KEY="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"
     }
     stage("Checkout"){
         dir("o2-paas"){
