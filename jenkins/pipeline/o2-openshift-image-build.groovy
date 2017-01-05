9a89f659dc975983daf4b194761241325abe9d9e
oscarkramer <okramer@radiantblue.com>
2017-01-05 15:26:38 -0500

    Added pw for modapps registry

--- jenkins/pipeline/o2-openshift-image-build.groovy
+++ jenkins/pipeline/o2-openshift-image-build.groovy
@@ -11,7 +11,7 @@ node("master"){
         echo "Using ModApps account"
         env.USE_C2S_ACCOUNT="false"
         env.DOCKER_REGISTRY_URI="docker-registry-default.o2.radiantbluecloud.com"
-        env.DOCKER_REGISTRY_PW="oQkGyJA7G3daTUDPKNZKRo_dRJ1ANkkRske99zUkBGA"
+        env.DOCKER_REGISTRY_PW="2fOqq6fPnmNqXTrK-tSztJZE2d5u2MJmMRsI3bKrxPU"
         env.OPENSHIFT_PROJECT_PATH="/${OSSIM_GIT_BRANCH}"
     }
     stage("Checkout"){
