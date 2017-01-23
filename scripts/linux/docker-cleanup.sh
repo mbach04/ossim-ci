6f4ceee66ab8916c7c7e0d12943309a32cb1d862
Garrett Potts <gpotts@radiantblue.com>
2017-01-23 12:58:54 -0500

    Added empty checks before calling removes or you will get errors

--- scripts/linux/docker-cleanup.sh
+++ scripts/linux/docker-cleanup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-docker rm $(docker ps -q -f status=exited)
-docker volume rm $(docker volume ls -qf dangling=true)
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+ITEMS_TO_REMOVE=$(docker ps -q -f status=exited)
+if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
+   docker rm ${ITEMS_TO_REMOVE}
+fi
+
+ITEMS_TO_REMOVE=$(docker volume ls -qf dangling=true)
+if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
+   docker volume rm ${ITEMS_TO_REMOVE}
+fi
+
+ITEMS_TO_REMOVE=$(docker images --filter "dangling=true" -q --no-trunc)
+if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
+   docker rmi ${ITEMS_TO_REMOVE}
+fi
+ 
