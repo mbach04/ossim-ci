68386f1c5c2d45dbb3851ac892a8cf35fa58fe96
Garrett Potts <gpotts@radiantblue.com>
2017-01-23 15:23:14 -0500

    Added checks for <non> tags

--- scripts/linux/docker-cleanup.sh
+++ scripts/linux/docker-cleanup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
+DOCKER_COMMAND=sudo docker
 ITEMS_TO_REMOVE=$(docker ps -q -f status=exited)
 if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
-   docker rm ${ITEMS_TO_REMOVE}
+   $DOCKER_COMMAND rm ${ITEMS_TO_REMOVE}
 fi
 
 ITEMS_TO_REMOVE=$(docker volume ls -qf dangling=true)
 if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
-   docker volume rm ${ITEMS_TO_REMOVE}
+   $DOCKER_COMMAND volume rm ${ITEMS_TO_REMOVE}
 fi
 
 ITEMS_TO_REMOVE=$(docker images --filter "dangling=true" -q --no-trunc)
 if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
-   docker rmi ${ITEMS_TO_REMOVE}
+   $DOCKER_COMMAND rmi ${ITEMS_TO_REMOVE}
 fi
  
+ITEMS_TO_REMOVE=$(sudo docker images | grep "<none>" | awk '{print $3}')
+if [ "${ITEMS_TO_REMOVE}" != "" ] ; then
+   $DOCKER_COMMAND rmi ${ITEMS_TO_REMOVE}
+fi
