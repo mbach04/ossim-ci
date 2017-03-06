caf4fc69e8091cea82e18a0fcf7d688a63aeb5c6
Chris Dowin <cdowin@radiantblue.com>
2017-03-06 14:36:23 -0500

    Removed references to OC2S environment

--- jenkins/pipeline/o2-image-build.groovy
+++ jenkins/pipeline/o2-image-build.groovy
@@ -35,30 +35,9 @@ node("master"){
       stage("Build")
       {
         // The base images are pulled from the modapps registry for building.
-        // NOTE: The builds necessarily pull base images (o2-base, o2-ossim) from
-        // a common registry for both oc2s and modapps destinations. Therefore,
-        // all builds (oc2s and modapps) need to always push images to the modapps
-        // registry to avoid stale base images for subsequent builds. In any case,
-        // the jenkins builds will be defaulting to "all" which makes this moot.
         setupModappsRegistry()
         buildImages()
         pushImages()
-
-        switch(EXPORT_REGISTRY) {
-          case "modapps":
-            // Nothing to do here since already done above.
-          break
-          case "oc2s":
-            setupC2SRegistry()
-            buildImages()
-            pushImages()
-          break
-          default:
-            // default = all
-            setupC2SRegistry()
-            buildImages()
-            pushImages()
-          break
       }
       if (SKIP_EXPORT_STAGE=="false")
       {
