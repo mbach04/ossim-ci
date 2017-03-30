6e251e8984c9f1592cb7a091093332c24f3f344d
Garrett Potts <gpotts@radiantblue.com>
2017-03-30 09:56:43 -0400

    Added if check

--- scripts/linux/oldmar-build.sh
+++ scripts/linux/oldmar-build.sh
@@ -7,12 +7,12 @@ popd >/dev/null
 
 source $OSSIMCI_SCRIPT_DIR/ossim-env.sh
 
-if [ ! -f "$OSSIM_DEV_HOME/joms-$OSSIM_VERSION.jar" ]; then
-   echo "ERROR: $OSSIM_DEV_HOME/joms-$OSSIM_VERSION.jar is not found in the install artifact and OMAR can't be built."
+if [ ! -f "$OSSIM_DEV_HOME/joms-$OSSIM_VERSION-$OSSIM_VERSION_TAG.jar.jar" ]; then
+   echo "ERROR: $OSSIM_DEV_HOME/joms-$OSSIM_VERSION-$OSSIM_VERSION_TAG.jar is not found in the install artifact and OMAR can't be built."
    exit 1
 fi
 # make sure the joms jar is in the local maven repo
-mvn install:install-file -Dfile=$OSSIM_DEV_HOME/joms-$OSSIM_VERSION.jar -DgroupId=org.ossim -DartifactId=joms -Dversion=$OSSIM_VERSION -Dpackaging=jar
+#mvn install:install-file -Dfile=$OSSIM_DEV_HOME/joms-$OSSIM_VERSION.jar -DgroupId=org.ossim -DartifactId=joms -Dversion=$OSSIM_VERSION -Dpackaging=jar
 if [ $? -ne 0 ]; then
  echo; echo "ERROR: MVN install failed for joms."
  exit 1
